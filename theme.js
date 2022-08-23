d79250494a6d56bb83db10baf21b75eab7b6b36a
Lukas Kral <lukywill16@gmail.com>
2022-08-23 11:08:01 +0200

    use id, not class
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- theme.js
+++ theme.js
@@ -2,9 +2,9 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$('.jenkins-head-icon').ready(function() {
-    $('.jenkins-head-icon').attr('src', 'https://design.jboss.org/strimzi/logo/final/strimzilogomark_default_512px.png')
+$('#jenkins-head-icon').ready(function() {
+    $('#jenkins-head-icon').attr('src', 'https://design.jboss.org/strimzi/logo/final/strimzilogomark_default_512px.png')
 });
-$('jenkins-name-icon').ready(function() {
-    $('jenkins-name-icon').attr('src', 'https://github.com/im-konge/jenkins-theme/raw/main/coollogo_com-254835774.png')
+$('#jenkins-name-icon').ready(function() {
+    $('#jenkins-name-icon').attr('src', 'https://github.com/im-konge/jenkins-theme/raw/main/coollogo_com-254835774.png')
 });
