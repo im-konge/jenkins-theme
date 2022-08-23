2257c28f86d0b662ebf21e8257fee6348a255521
Lukas Kral <lukywill16@gmail.com>
2022-08-23 11:34:43 +0200

    try
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- theme.js
+++ theme.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 // Immediately-invoked function expression
 (function() {
   // Load the script
